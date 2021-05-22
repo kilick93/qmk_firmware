5fe9cbada74f7ab94dbedb084f944421db1c403b
DESKTOP-GK2IBO5\rkili <rkilick@gmail.com>
2021-05-22 20:26:42 +0200

    Correction keymap bibob

--- keyboards/bm60rgb_iso/keymaps/bibob/keymap.c
+++ keyboards/bm60rgb_iso/keymaps/bibob/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT_60_iso_arrow(
         RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
         _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC__MUTE, KC_VOLU, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, _______,
         _______, _______, _______,                            _______,                   _______, _______, KC_HOME, KC_PGDN, KC_END
     ),
